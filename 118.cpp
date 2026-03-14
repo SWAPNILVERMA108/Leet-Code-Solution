5150b20d1db4dd085b80bf975be2b81ab87ee3bb
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-14 23:32:23 +0530

    pascals-triangle

---
+++ 118.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+   
+    vector<int> generateRow(int row) {
+        long long ans = 1;
+        vector<int> ansRow;
+
+        ansRow.push_back(1); 
+
+        for(int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.push_back(ans);
+        }
+
+        return ansRow;
+    }
+
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+
+        for(int row = 1; row <= numRows; row++) {
+            triangle.push_back(generateRow(row));
+        }
+
+        return triangle;
+    }
+};
