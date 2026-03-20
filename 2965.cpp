59dd1d34647e31bacf88f4f5e34e349793389478
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-20 19:02:55 +0530

    find missing and repeted value

---
+++ 2965.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+
+        vector<int> freq(n*n +1,0);
+
+        for(auto row: grid){
+            for(auto val :row){
+                freq[val]++;
+            }
+        }
+        int repeated =-1;
+        int missing = -1;
+        for(int i=1;i<=n*n;i++){
+            if(freq[i]==2){
+                repeated =i;
+            }
+            if(freq[i]==0){
+                missing = i;
+            }
+        }
+        return {repeated , missing};
+
+
+
+    }
+};
