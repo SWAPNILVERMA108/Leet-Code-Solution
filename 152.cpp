b545ab3ae05bc9a29f07f1875a7590d8293dad22
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-25 17:32:28 +0530

    maximum product sum (optimal approch)

--- 152.cpp
+++ 152.cpp
@@ -1,20 +1,45 @@
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int maxi = nums[0];
+//         int mini = nums[0];
+//         int ans = nums[0];
+//         for(int i =1;i<nums.size();i++){
+//             if(nums[i]<0){
+//                 swap(maxi,mini);
+//             }
+//             maxi = max(nums[i],maxi*nums[i]);
+
+//             mini = min(nums[i],mini*nums[i]);   
+//             ans = max(ans,maxi);
+//         }
+//         return ans;
+
+        
+//     }
+// };
+
+
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxi = nums[0];
-        int mini = nums[0];
-        int ans = nums[0];
-        for(int i =1;i<nums.size();i++){
-            if(nums[i]<0){
-                swap(maxi,mini);
-            }
-            maxi = max(nums[i],maxi*nums[i]);
+        int ans = INT_MIN;
+        int pre = 1;
+        int suff = 1;
+        int n = nums.size();
 
-            mini = min(nums[i],mini*nums[i]);   
-            ans = max(ans,maxi);
+        for (int i = 0; i < n; i++) {
+
+            if (pre == 0)
+                pre = 1;
+            if (suff == 0)
+                suff = 1;
+
+            pre = pre * nums[i];
+            suff = suff * nums[n - i - 1];
+
+            ans = max(ans, max(pre, suff));
         }
         return ans;
-
-        
     }
 };
