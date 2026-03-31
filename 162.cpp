d11ef05be8d2d2504787f558d22197822c5ccc65
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-03-31 11:29:15 +0530

    Find Peek Element (optimal approach)

--- 162.cpp
+++ 162.cpp
@@ -1,3 +1,40 @@
+// 162. Find Peak Element (Optimized Approach)
+
+// time complexity : O(log n)
+// space complexity : O(1)
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1)
+            return 0;
+
+        if (nums[0] > nums[1])
+            return 0;
+        if (nums[n - 1] > nums[n - 2])
+            return n - 1;
+
+        int low = 1;
+        int high = n - 2;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+
 // Find Peak Element (Brute Force Approach)
 
 // Instructions : Not for leetcode submission, just for understanding the concept of brute force approach.
