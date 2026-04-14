b284c1713f51d731d1d22a8dda60c73ffaf1b56d
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-04-14 20:01:49 +0530

    longest-substring-without-repeating-characters

---
+++ 3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> hash(256, -1);
+        
+        int l = 0, maxLen = 0;
+        
+        for (int r = 0; r < s.size(); r++) {
+            
+            if (hash[s[r]] >= l) {
+                l = hash[s[r]] + 1;
+            }
+            
+            hash[s[r]] = r;
+            
+            maxLen = max(maxLen, r - l + 1);
+        }
+        
+        return maxLen;
+    }
+};
