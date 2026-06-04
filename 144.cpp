184f8a3824a24cde6bdd590fa0f51526f1626476
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-06-04 18:14:45 +0530

    recursive and iterative approch

--- 144.cpp
+++ 144.cpp
@@ -27,3 +27,35 @@ return ans;
         
     }
 };
+
+
+
+// iterative approch 
+
+vector<int> preorderTraversal(TreeNode* root) {
+
+
+vector<int>ans;
+if(root==nullptr){
+    return ans;
+}
+stack<TreeNode*>st;
+
+st.push(root);
+while(!st.empty()){
+    TreeNode* node = st.top();
+    st.pop();
+    ans.push_back(node->val);
+    if(node->right !=nullptr){
+        st.push(node->right);
+
+    }
+    if(node->left !=nullptr){
+        st.push(node->left);
+    }
+
+}
+return ans;
+        
+    }
+};
