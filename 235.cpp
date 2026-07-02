1ad9b4bc8924452e08aaa8191da1facb39290cea
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-07-02 19:14:12 +0530

    LCA of BST

---
+++ 235.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// recursive approch
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+//     {
+//         if(p->val<root->val && q->val <root->val){
+//             return lowestCommonAncestor(root->left,p,q);
+//         }
+//         if(p->val>root->val && q->val >root->val){
+//             return lowestCommonAncestor(root->right,p,q);
+//         }
+//         return root;
+//     }
+// };
+
+// iterative approch
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while (root) {
+            if (p->val < root->val && q->val < root->val) {
+                root = root->left;
+            } else if (p->val > root->val && q->val > root->val) {
+                root = root->right;
+            } else {
+                return root;
+            }
+        }
+        return nullptr;
+    }
+};
