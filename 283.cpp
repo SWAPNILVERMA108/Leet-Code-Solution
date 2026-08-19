bca49e6a0da5a45bb3b924a175921a6803683311
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-08-19 23:02:38 +0530

    cinema seat allocation

--- 283.cpp
+++ 283.cpp
@@ -38,3 +38,22 @@ public:
     }
 };
 
+
+
+
+// optimal solution without using extra space
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int insertPos=0;
+        for(int x:nums){
+            if(x!=0){
+                nums[insertPos++]=x;
+            }
+        }
+        while(insertPos<nums.size()){
+            nums[insertPos++]=0;
+        }
+        
+    }
+};
