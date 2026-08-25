8c39c9a977aa50845750e0c79a4ab70e5317f6c6
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-08-25 23:50:43 +0530

    NO of 1 sets

---
+++ 191.cpp
@@ -0,0 +1,12 @@
+lass Solution {
+public:
+    int hammingWeight(int n) {
+        int count= 0;
+        while(n){
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+        
+    }
+};
