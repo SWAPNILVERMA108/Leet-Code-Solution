19a87d481c443fd994c8b2c8da29ec23c621e296
SWAPNIL VERMA <swapnilverma109@gmail.com>
2026-08-27 17:18:33 +0530

    ugly no 3

---
+++ 1201.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    using ll = long long;
+    ll lcm(ll x, ll y) { return x / gcd(x, y) * y; }
+
+    int nthUglyNumber(int n, int a, int b, int c) {
+        ll ab = lcm(a, b);
+        ll bc = lcm(b, c);
+        ll ac = lcm(a, c);
+        ll abc = lcm(ab, c);
+
+        ll left = 1;
+        ll right = 2e9;
+
+        while (left < right) {
+            ll mid = left + (right - left) / 2;
+
+            ll cnt = mid / a + mid / b + mid / c - mid / ab - mid / bc -
+                     mid / ac + mid / abc;
+
+            if (cnt >= n) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+};
